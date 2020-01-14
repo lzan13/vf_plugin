65cc3b395ddf51d88135f1e32c59c086e91660a2
liulizheng <liulizheng@weidian.com>
2020-01-14 18:08:00 +0800

    添加自定义日志输出工具类

---
+++ lib/src/utils/vf_utils.dart
@@ -0,0 +1,1 @@
+export 'vf_log.dart';
--- lib/vf_plugin.dart
+++ lib/vf_plugin.dart
@@ -1,4 +1,5 @@
 // 资源导出
 export 'src/constants/vf_constants.dart';
 export 'src/plugin/vf_plugin.dart';
+export 'src/utils/vf_utils.dart';
 export 'src/widget/vf_widgets.dart';
